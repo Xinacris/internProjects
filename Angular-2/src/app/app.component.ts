1027f6cac9ab0241447cfe7466e1168ea61456f9
Mustafa Ali Eren <34250573+Xinacris@users.noreply.github.com>
2022-07-19 16:28:37 +0300

    Updates through lessons.
    Same projects as Angular-1, but updated versions.

---
+++ Angular-2/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { style } from '@angular/animations';
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'my-app';
+  index=0
+  
+  selectedContent(val:any){
+    this.index=val;
+  }
+}
